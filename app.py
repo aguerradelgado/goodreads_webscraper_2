7309c22e5ec90ea2bff88754e5b1c04ee1d914de
Alessandra Guerra <aleg101@live.com>
2022-11-25 10:25:44 -0500

    added in sessions

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import urllib.request
 from bs4 import BeautifulSoup
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, session
+from flask_session import Session
 import sqlite3
 import datetime
 import time
@@ -8,6 +9,9 @@ import time
 # comment for testing
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 #for db
 app.secret_key = "secret key"
@@ -51,7 +55,22 @@ def book_page(searchterms):
 
 @app.route('/')
 def home():
+    # check if the users exist or not
+    if not session.get("name"):
+        # if not there in the session then redirect to the login page
+        return redirect("/login")
     return render_template('index.html')
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    # if form is submited
+    if request.method == "POST":
+        # record the user name
+        session["name"] = request.form.get("name")
+        # redirect to the main page
+        return redirect("/")
+    return render_template("login.html")
+
 
 @app.route('/index', methods=["POST"])
 def index2():
@@ -112,6 +131,11 @@ def index2():
         else:
             print("In Book DB")
         return render_template("result.html", book_input=book_input, authors=author, image=cover, ratings=rating, books=books_give, words=titles_give)
+
+    @app.route("/logout")
+    def logout():
+        session["name"] = None
+        return redirect("/")
 
 
 @app.route('/shelf', methods=('GET', 'POST'))
